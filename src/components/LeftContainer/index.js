5826e0f265438e14b1d3afa3a38fd40872369910
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-05-14 23:31:15 +0530

    Splitted code and made MovieCard component

--- src/components/LeftContainer/index.js
+++ src/components/LeftContainer/index.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../contexts/UserContext";
 import { API_KEY } from "../../keys";
+import MovieCard from "../MovieCard/MovieCard";
 
-function LeftContainer({ movies, IMAGE_PATH }) {
+function LeftContainer({ movies }) {
+  const { favourites, dispatch } = useContext(UserContext);
   const [selectedMovie, setSelectedMovie] = useState("");
   const [reviews, setReviews] = useState(null);
 
   const REVIEWS_API = `https://api.themoviedb.org/3/movie/${selectedMovie.id}/reviews?api_key=${API_KEY}&language=en-US&page=1`;
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280";
 
   useEffect(() => {
     selectedMovie &&
@@ -14,18 +18,14 @@ function LeftContainer({ movies, IMAGE_PATH }) {
         .then((data) => setReviews(data.results));
   }, [selectedMovie]);
 
-  const setColors = (vote) => {
-    if (vote >= 8) {
-      return "green";
-    } else if (vote >= 6) {
-      return "orange";
+  const favouriteHandler = (id) => {
+    const foundMovie = favourites.filter((favourite) => favourite.id === id);
+    // console.log(foundMovie);
+    if (foundMovie.length === 0) {
+      dispatch({ type: "ADD_FAV", favourite: { ...selectedMovie } });
     } else {
-      return "red";
+      dispatch({ type: "DELETE_FAV", id: selectedMovie.id });
     }
-  };
-
-  const clickHandler = (id) => {
-    setSelectedMovie(movies.filter((movie) => movie.id === id)[0]);
   };
 
   return (
@@ -42,7 +42,9 @@ function LeftContainer({ movies, IMAGE_PATH }) {
             }
           />
           <div className="left-container__main--extra">
-            <button>❤️</button>
+            <button onClick={() => favouriteHandler(selectedMovie.id)}>
+              ❤️
+            </button>
             <div className="left-container__main--extra-reviews">
               <div style={{ marginLeft: "1rem" }}>
                 Reviews 👉 {reviews && reviews.length}
@@ -62,42 +64,13 @@ function LeftContainer({ movies, IMAGE_PATH }) {
       ) : null}
       {movies.length > 0 ? (
         movies.map((movie) => (
-          <div key={movie.id} className="left-container__movies">
-            <img
-              className="left-container__movies--cover"
-              src={
-                movie.poster_path
-                  ? IMAGE_PATH + movie.poster_path
-                  : "https://images.unsplash.com/photo-1535016120720-40c646be5580?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-              }
-              alt={movie.title}
-            />
-            <div
-              onClick={() => clickHandler(movie.id)}
-              className="left-container__movies--info"
-            >
-              <h3>
-                {movie.title.length > 10
-                  ? movie.title.substring(0, 10) + "..."
-                  : movie.title}
-              </h3>
-              <h3
-                className={`left-container__movies--info-rating ${setColors(
-                  movie.vote_average
-                )}`}
-              >
-                {movie.vote_average}
-              </h3>
-            </div>
-            <div className="left-container__movies--hover">
-              <div>
-                <div className="left-container__movies--hover-title">
-                  {movie.title}
-                </div>
-                “<em>{movie.overview}</em>”
-              </div>
-            </div>
-          </div>
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            IMAGE_PATH={IMAGE_PATH}
+            movies={movies}
+            setSelectedMovie={setSelectedMovie}
+          />
         ))
       ) : (
         <div style={{ color: "white", fontSize: "1.6rem" }}>
---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+function MovieCard({ movie, IMAGE_PATH, movies, setSelectedMovie }) {
+  const setColors = (vote) => {
+    if (vote >= 8) {
+      return "green";
+    } else if (vote >= 6) {
+      return "orange";
+    } else {
+      return "red";
+    }
+  };
+
+  const clickHandler = (id) => {
+    setSelectedMovie(movies.filter((movie) => movie.id === id)[0]);
+  };
+
+  return (
+    <div key={movie.id} className="left-container__movies">
+      <img
+        className="left-container__movies--cover"
+        src={
+          movie.poster_path
+            ? IMAGE_PATH + movie.poster_path
+            : "https://images.unsplash.com/photo-1535016120720-40c646be5580?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+        }
+        alt={movie.title}
+      />
+      <div
+        onClick={() => clickHandler(movie.id)}
+        className="left-container__movies--info"
+      >
+        <h3>
+          {movie.title.length > 10
+            ? movie.title.substring(0, 10) + "..."
+            : movie.title}
+        </h3>
+        <h3
+          className={`left-container__movies--info-rating ${setColors(
+            movie.vote_average
+          )}`}
+        >
+          {movie.vote_average}
+        </h3>
+      </div>
+      <div className="left-container__movies--hover">
+        <div>
+          <div className="left-container__movies--hover-title">
+            {movie.title}
+          </div>
+          “<em>{movie.overview}</em>”
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
