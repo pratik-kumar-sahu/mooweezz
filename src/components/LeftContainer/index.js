7a4608fce0db524155ca921dba878b06058d21eb
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-05-16 20:30:22 +0530

    Minor changes in UI

--- src/components/LeftContainer/index.js
+++ src/components/LeftContainer/index.js
@@ -1,21 +1,31 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import { API_KEY } from "../../keys";
+import FavouriteCard from "../FavouriteCard/FavouriteCard";
 import MovieCard from "../MovieCard/MovieCard";
 
 function LeftContainer({ movies, selectedMovie, setSelectedMovie }) {
   const { favourites, dispatch } = useContext(UserContext);
   const [reviews, setReviews] = useState(null);
+  const [recommends, setRecommends] = useState(null);
   const dummy = useRef();
 
   const REVIEWS_API = `https://api.themoviedb.org/3/movie/${selectedMovie.id}/reviews?api_key=${API_KEY}&language=en-US&page=1`;
   const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280";
+  const RECOMMENDS = `https://api.themoviedb.org/3/movie/${selectedMovie.id}/recommendations?api_key=${API_KEY}&language=en-US&page=1`;
 
   useEffect(() => {
     selectedMovie &&
       fetch(REVIEWS_API)
         .then((res) => res.json())
         .then((data) => setReviews(data.results));
+
+    selectedMovie &&
+      fetch(RECOMMENDS)
+        .then((res) => res.json())
+        .then((data) => {
+          setRecommends(data.results);
+        });
   }, [selectedMovie]);
 
   const scrollOnTop = () => {
@@ -37,44 +47,63 @@ function LeftContainer({ movies, selectedMovie, setSelectedMovie }) {
 
   return (
     <div id="left-container" className="left-container">
-      {selectedMovie ? (
-        <div className="left-container__main" ref={dummy}>
-          <img
-            className="left-container__main--cover"
-            alt={selectedMovie.title}
-            src={
-              selectedMovie.poster_path
-                ? IMAGE_PATH + selectedMovie.poster_path
-                : "https://images.unsplash.com/photo-1535016120720-40c646be5580?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-            }
-          />
-          <div className="left-container__main--extra">
-            <div className="left-container__main--extra-add">
-              <button
-                className="left-container__main--extra-add--btn"
-                onClick={() => favouriteHandler(selectedMovie.id)}
-              >
-                {favourites.filter((e) => e.data.id === selectedMovie.id).length
-                  ? "Remove ❤️"
-                  : "Add ❤️"}
-              </button>
-            </div>
-            <div className="left-container__main--extra-reviews">
-              <div style={{ marginLeft: "1rem" }}>
-                Reviews 👉 {reviews && reviews.length}
-              </div>
-              {reviews &&
-                reviews.map((review) => (
-                  <div className="reviews" key={review.id}>
-                    <div className="reviews-author">{review.author}</div>
-                    <div className="reviews-content">
-                      <em>{review.content}</em>
-                    </div>
-                  </div>
-                ))}
-            </div>
+      <div className='hide-it' ref={dummy}></div>
+      {selectedMovie && recommends ? (
+        <div className="left-container__recommends">
+          <h3>RECOMM...</h3>
+          <div>
+            {recommends &&
+              recommends.map((recommend) => (
+                <FavouriteCard
+                  key={recommend.id}
+                  favMovie={recommend}
+                  setSelectedMovie={setSelectedMovie}
+                />
+              ))}
           </div>
         </div>
+      ) : null}
+      {selectedMovie ? (
+        <>
+          <div className="left-container__main">
+            <img
+              className="left-container__main--cover"
+              alt={selectedMovie.title}
+              src={
+                selectedMovie.poster_path
+                  ? IMAGE_PATH + selectedMovie.poster_path
+                  : "https://images.unsplash.com/photo-1535016120720-40c646be5580?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+              }
+            />
+            <div className="left-container__main--extra">
+              <div className="left-container__main--extra-add">
+                <button
+                  className="left-container__main--extra-add--btn"
+                  onClick={() => favouriteHandler(selectedMovie.id)}
+                >
+                  {favourites.filter((e) => e.data.id === selectedMovie.id)
+                    .length
+                    ? "Remove ❤️"
+                    : "Add ❤️"}
+                </button>
+              </div>
+              <div className="left-container__main--extra-reviews">
+                <div style={{ marginLeft: "1rem" }}>
+                  Reviews 👉 {reviews && reviews.length}
+                </div>
+                {reviews &&
+                  reviews.map((review) => (
+                    <div className="reviews" key={review.id}>
+                      <div className="reviews-author">{review.author}</div>
+                      <div className="reviews-content">
+                        <em>{review.content}</em>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </>
       ) : null}
       {movies.length > 0 ? (
         movies.map((movie) => (
